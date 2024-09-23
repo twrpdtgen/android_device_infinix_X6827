50d9e6fa7038c2e5740a40ae7620e6e3d58e97e2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-23 06:44:19 +0200

    X6827: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: HOT 20S Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_X6827.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_X6827-user \
+    omni_X6827-userdebug \
+    omni_X6827-eng
